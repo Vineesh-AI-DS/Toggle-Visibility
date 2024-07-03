c7c66c78469816a861d7ce22fffb552da13b9c4b
Vineesh-AI-DS <vineeshxd3@gmail.com>
2024-07-03 14:04:05 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+//Vineesh - 212221230122
+export default defineConfig({
+  plugins: [react()],
+  base: 'Toggle-Visibility-Feature-D2-A1'
+})
